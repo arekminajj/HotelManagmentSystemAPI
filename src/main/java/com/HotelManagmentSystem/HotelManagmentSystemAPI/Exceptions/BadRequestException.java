62fae01c09f6e6ae768ff1f405cbb1268cc1fd8a
Arek Cios <arek123113@gmail.com>
2024-03-20 23:46:00 +0100

    Add BadRequestException

--- src/main/java/com/HotelManagmentSystem/HotelManagmentSystemAPI/Exceptions/BadRequestException.java
+++ src/main/java/com/HotelManagmentSystem/HotelManagmentSystemAPI/Exceptions/BadRequestException.java
@@ -1,2 +1,8 @@
-package com.HotelManagmentSystem.HotelManagmentSystemAPI.Exceptions;public class BadRequestException {
-}
+package com.HotelManagmentSystem.HotelManagmentSystemAPI.Exceptions;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {}
