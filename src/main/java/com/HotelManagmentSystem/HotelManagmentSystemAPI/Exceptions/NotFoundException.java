6455a786449dc643c35c9ac2110596a08a8cc65c
Arek Cios <arek123113@gmail.com>
2024-03-20 23:23:03 +0100

    Add NotFoundException class

---
+++ src/main/java/com/HotelManagmentSystem/HotelManagmentSystemAPI/Exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.HotelManagmentSystem.HotelManagmentSystemAPI.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {}
