df6b418eb5082a83e93b8758957d084e66b3d48b
Arek Cios <arek123113@gmail.com>
2024-03-20 23:47:05 +0100

    Add CRUD functionality to the Room controller

--- src/main/java/com/HotelManagmentSystem/HotelManagmentSystemAPI/controller/RoomController.java
+++ src/main/java/com/HotelManagmentSystem/HotelManagmentSystemAPI/controller/RoomController.java
@@ -1,48 +1,76 @@
 package com.HotelManagmentSystem.HotelManagmentSystemAPI.controller;
 
+import com.HotelManagmentSystem.HotelManagmentSystemAPI.Exceptions.BadRequestException;
+import com.HotelManagmentSystem.HotelManagmentSystemAPI.Exceptions.NotFoundException;
 import com.HotelManagmentSystem.HotelManagmentSystemAPI.repository.RoomRepository;
 import com.HotelManagmentSystem.HotelManagmentSystemAPI.model.Room;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path="/room")
 public class RoomController {
 
     //private RoomService roomService;
     @Autowired
     private RoomRepository roomRepository;
 
-    //public RoomController(RoomRepository roomRepository) {
-    //    this.roomRepository = roomRepository;
-    //}
+    @GetMapping(path="/all")
+    public ResponseEntity<Iterable<Room>> getAlRooms() {
+        Iterable<Room> rooms = roomRepository.findAll();
+        return new ResponseEntity<Iterable<Room>>(rooms, HttpStatus.OK);
+    }
 
+    @GetMapping(path="/{id}")
+    public ResponseEntity<Room> GetRoomById(@PathVariable int id) {
+        Optional<Room> roomOptional = roomRepository.findById(id);
+
+        if (roomOptional.isPresent()) {
+            return ResponseEntity.ok(roomOptional.get());
+        } else {
+            throw new NotFoundException();
+        }
+    }
 
     @PostMapping(path="/add") // Map ONLY POST Requests
-    public @ResponseBody String addNewRoom () {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        Room room = new Room();
-
-        room.setNumber(12);
-        room.setDescription("jakis opis pokoju");
-        room.setCapacity(3);
-        room.setFloor(4);
-        room.setPrice(200);
-
-
-        roomRepository.save(room);
-        return "Saved";
+    public ResponseEntity<Room> addNewRoom(@RequestBody Room room) {
+        Room savedRoom = roomRepository.save(room);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedRoom);
     }
 
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<Room> getAllUsers() {
-        // This returns a JSON or XML with the users
-        return roomRepository.findAll();
-    }
+    @PutMapping(path="/{id}")
+    public ResponseEntity<Room> updateRoom(@RequestBody Room updatedRoom,@PathVariable int id)
+    {
+        Optional<Room> existingRoomOptional = roomRepository.findById(id);
 
+        if (existingRoomOptional.isPresent()) {
+            Room existingRoom = existingRoomOptional.get();;
+
+            existingRoom.setDescription(updatedRoom.getDescription());
+            existingRoom.setCapacity(updatedRoom.getCapacity());
+            existingRoom.setPrice(updatedRoom.getPrice());
+            existingRoom.setFloor(updatedRoom.getFloor());
+            existingRoom.setPhotos(updatedRoom.getPhotos());
+
+            Room savedRoom = roomRepository.save(existingRoom);
+            return ResponseEntity.ok(savedRoom);
+        } else {
+            throw new BadRequestException();
+        }
+    }
+    @DeleteMapping(path="/{id}")
+    public ResponseEntity<String> deleteRoom(@PathVariable int id) {
+        Optional<Room> roomOptional = roomRepository.findById(id);
+        if(roomOptional.isPresent()) {
+            roomRepository.deleteById(id);
+            return ResponseEntity.ok("Room of id: " + id + " has been deleted");
+        }
+        else {
+            throw new BadRequestException();
+        }
+    }
 }
